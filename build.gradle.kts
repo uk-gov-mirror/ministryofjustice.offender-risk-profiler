a65e4a2f69d131de230f75366f1ea4a47671eeac
raghavender.tangallapally <raghavender.tangallapally@justice.gov.uk>
2025-06-06 10:06:53 +0100

    SI-1647 new code changes

--- build.gradle.kts
+++ build.gradle.kts
@@ -119,9 +119,9 @@ dependencies {
     exclude("org.apache.tomcat.embed", "tomcat-embed-websocket")
     exclude("org.apache.tomcat.embed", "tomcat-embed-el")
   }
-  implementation("org.apache.tomcat.embed", "tomcat-embed-core", "9.0.99")
-  implementation("org.apache.tomcat.embed", "tomcat-embed-websocket", "9.0.99")
-  implementation("org.apache.tomcat.embed", "tomcat-embed-el", "9.0.99")
+  implementation("org.apache.tomcat.embed", "tomcat-embed-core", "10.1.18")
+  implementation("org.apache.tomcat.embed", "tomcat-embed-websocket", "10.1.18")
+  implementation("org.apache.tomcat.embed", "tomcat-embed-el", "10.1.18")
 
   implementation("com.microsoft.azure:applicationinsights-spring-boot-starter:2.6.4")
   implementation("com.microsoft.azure:applicationinsights-logging-logback:2.6.4")
